270356d2a33de907c09909cb3b2c8a1e1db253af
young-trigold <cluscandlot63@gmail.com>
2021-06-05 14:26:55 +0800

    Add files via upload

--- socket_programming/udp_socket/UDPClient.py
+++ socket_programming/udp_socket/UDPClient.py
@@ -1,17 +1,9 @@
 from socket import *
-
-serverName = 'localhost' # 服务器为本地主机
-
-serverPort = 12000 # 端口号
-
-clientSocket = socket(AF_INET, SOCK_DGRAM) # 新建 UDP 客户套接字
-
+serverName = 'localhost'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
 message = input('输入一个全是小写的句子：')
-
-clientSocket.sendto(message.encode(), (serverName, serverPort)) # 发送消息
-
-rcvMessage, serverAddress = clientSocket.recvfrom(2048)
-
-print(rcvMessage.decode ())
-
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
 clientSocket.close()
--- socket_programming/udp_socket/UDPServer.py
+++ socket_programming/udp_socket/UDPServer.py
@@ -1,16 +1,9 @@
 from socket import *
-
-serverPort = 12000 # 服务端端口
-
-serverSocket = socket(AF_INET, SOCK_DGRAM) # 新建 UDP socket
-
-serverSocket.bind(('', serverPort)) # 绑定 12000 端口
-
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
 print('服务器已经准备好接收了！')
-
 while True:
-  rcvMessage, clientAddress = serverSocket.recvfrom(2048) # 储存客户端发来的信息
-  
-  sendMessage  = rcvMessage.decode().upper() # 大写信息
-
-  serverSocket.sendto(sendMessage.encode(), clientAddress) # 发送信息
+  message, clientAddress = serverSocket.recvfrom(2048)
+  modifiedMessage = message.decode().upper()
+  serverSocket.sendto(modifiedMessage.encode(), clientAddress)
