a6cdc57166b3677e09fff29f58fdca6dce7f1cfa
young-trigold <cluscandlot63@gmail.com>
2021-07-30 16:41:44 +0800

    updated

--- socket_propramming/web_server/OrignWebServer.py
+++ socket_propramming/web_server/OrignWebServer.py
@@ -4,25 +4,25 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 # 准备一个欢迎套接字
 # 开始补全
 # 结束补全
-statusLineFor200 = 'HTTP/1.1 200 OK\n' # 构造 HTTP 响应报文中请求成功状态行
+statusLineFor200 = 'HTTP/1.1 200 OK' + CRLF # 构造 HTTP 响应报文中请求成功状态行
 statusLineFor404 = # 开始补全 # 结束补全 # 构造 HTTP 响应报文中找不到请求对象状态行
-newLine = '\n' # 空行
+CRLF = '\r\n' # 空行
 while True:
     print('服务器已启动！\n')
     connectionSocket, addr = # 开始补全 # 结束补全 # 建立连接
     try:
         message = # 开始补全 # 结束补全
         filename = message.split()[1] # 使用空格将 HTTP 请求报文分隔，并提取出请求对象 URL
-        print('请求行：' + message.split('\n')[0] + '\n') # 打印出 HTTP 请求报文中的请求行
+        print('请求行：' + message.split(CRLF)[0] + CRLF) # 打印出 HTTP 请求报文中的请求行
         content = open(filename[1:]).read() # 读取文件，构造 HTTP 响应报文实体体
         outputdata = # 开始补全 # 结束补全 # 构造 HTTP 响应报文
-        print('HTTP响应报文：' + outputdata + '\n')
+        print('HTTP响应报文：' + outputdata + CRLF)
         connectionSocket.send(outputdata.encode())
         connectionSocket.close()
         print('连接已关闭！\n')
     except IOError:
-        notFound = statusLineFor404 + newLine + open('404.html').read() # 构造 HTTP 响应报文
-        print('HTTP响应报文：' + notFound + '\n')
+        notFound = statusLineFor404 + CRLF + open('404.html').read() # 构造 HTTP 响应报文
+        print('HTTP响应报文：' + notFound + CRLF)
          # 开始补全 # 结束补全 # 发送 404 响应报文
         # 开始补全 # 结束补全 # 关闭连接
         print('连接已关闭！\n')
--- socket_propramming/web_server/WebServer.py
+++ socket_propramming/web_server/WebServer.py
@@ -4,25 +4,25 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 # 准备一个欢迎套接字
 serverSocket.bind(('', 6789))
 serverSocket.listen(1)
-statusLineFor200 = 'HTTP/1.1 200 OK\n' # 构造 HTTP 响应报文中请求成功状态行
-statusLineFor404 = 'HTTP/1.1 404 Not Found\n' # 构造 HTTP 响应报文中找不到请求对象状态行 
-newLine = '\n' # 空行
+statusLineFor200 = 'HTTP/1.1 200 OK' + CRLF # 构造 HTTP 响应报文中请求成功状态行
+statusLineFor404 = 'HTTP/1.1 404 Not Found' + CRLF # 构造 HTTP 响应报文中找不到请求对象状态行 
+CRLF = '\r\n' # 换行回车
 while True:
     print('服务器已启动！\n')
     connectionSocket, addr = serverSocket.accept() # 建立连接
     try:
         message =  connectionSocket.recv(1024).decode()
         filename = message.split()[1] # 使用空格将 HTTP 请求报文分隔，并提取出请求对象 URL
-        print('请求行：' + message.split('\n')[0] + '\n') # 打印出 HTTP 请求报文中的请求行
+        print('请求行：' + message.split(CRLF)[0] + CRLF) # 打印出 HTTP 请求报文中的请求行
         content = open(filename[1:]).read() # 读取文件，构造 HTTP 响应报文实体体
-        outputdata = statusLineFor200 + newLine + content # 构造 HTTP 响应报文
-        print('HTTP响应报文：' + outputdata + '\n')
+        outputdata = statusLineFor200 + CRLF + content # 构造 HTTP 响应报文
+        print('HTTP响应报文：' + outputdata + CRLF)
         connectionSocket.send(outputdata.encode())
         connectionSocket.close()
         print('连接已关闭！\n')
     except IOError:
-        notFound = statusLineFor404 + newLine + open('404.html').read() # 构造 HTTP 响应报文
-        print('HTTP响应报文：' + notFound + '\n')
+        notFound = statusLineFor404 + CRLF + open('404.html').read() # 构造 HTTP 响应报文
+        print('HTTP响应报文：' + notFound + CRLF)
         connectionSocket.send(notFound.encode()) # 发送 404 响应报文
         connectionSocket.close() # 关闭连接
         print('连接已关闭！\n')
